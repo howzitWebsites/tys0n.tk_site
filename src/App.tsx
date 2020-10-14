a4e6d41f0f0a47874492d33eb7beec791257d287
Callum <38749032+howzitcal@users.noreply.github.com>
2020-10-14 15:57:11 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{textAlign:"center"}}>
+        <h3>T3mplate is up!<span role="img" aria-label="rocket">🚀</span> You can smile now! <span role="img" aria-label="smile">😬</span></h3>
+        <hr/>
+        <h4>We are currently working on our site <span role="img" aria-label="fencer">🤺</span></h4>
+        <h4>Please check back soon! </h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
