7f48de2917614895896a69621c98861f5f77364c
Callum Fleming <howzitcallum@gmail.com>
2020-10-14 20:51:59 +0200

    feat(jumbotron): added jumbotron with picture and title WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
 
+import Jumbotron from "./components/Jumbotron";
+
+import tysonLanding from "./images/tyson-landing.jpeg";
+
 const App: React.FC = () => {
   return (
     <div className="App">
-
-<div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-  <strong className="font-bold">Holy smokes!</strong>
-  <span className="block sm:inline">Something seriously bad happened.</span>
-  <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-  </span>
-</div>
-
-      <input
-        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10"
-        type="email"
-        placeholder="jane@example.com"
-      />
+      <section className="flex justify-center m-2">
+        <div className="md:w-4/5 h-64">
+          <Jumbotron image={tysonLanding} title="This is Tys0n" fontSize="text-xl lg:text-2xl"/>
+        </div>
+      </section>
     </div>
   )
 }
---
+++ src/components/Jumbotron.tsx
@@ -0,0 +1,20 @@
+import React, {FC} from "react";
+
+interface IJumbotronProps {
+    image: string,
+    title: string,
+    fontSize: string
+};
+
+const Jumbotron: FC<IJumbotronProps> = ({image, title, fontSize}) => {
+    return (
+    <div className="h-full w-full">
+        <img src={image} className="object-cover w-full h-full rounded-lg shadow-lg" alt="Jumbotron"/>
+        <div className="flex justify-center text-center -mt-10">
+            <h1 className={`bg-yellow-500 w-1/2 p-5 rounded-lg shadow-lg ${fontSize}`}>{title}</h1>
+        </div>
+    </div>
+    );
+}
+
+export default Jumbotron;
