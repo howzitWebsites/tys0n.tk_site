648fafe457a9d8d2635e9f1da9f79469cf800cc7
Callum Fleming <howzitcallum@gmail.com>
2020-10-14 21:20:32 +0200

    feat(jumbotron): added gradient

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -11,7 +11,7 @@ const Jumbotron: FC<IJumbotronProps> = ({image, title, fontSize}) => {
     <div className="h-full w-full">
         <img src={image} className="object-cover w-full h-full rounded-lg shadow-lg" alt="Jumbotron"/>
         <div className="flex justify-center text-center -mt-10">
-            <h1 className={`bg-yellow-500 w-1/2 p-5 rounded-lg shadow-lg ${fontSize}`}>{title}</h1>
+            <h1 className={`bg-gradient-to-r from-teal-400 to-blue-500 w-1/2 p-5 rounded-lg shadow-lg ${fontSize}`}>{title}</h1>
         </div>
     </div>
     );
