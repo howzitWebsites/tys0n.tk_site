e59dc117cfcc218799f56cf9360ea45ee7ae129f
Callum Fleming <howzitcallum@gmail.com>
2020-10-15 08:04:36 +0200

    feat(jumbotron): testing multiple jumbotrons on the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,26 @@ import tysonLanding from "./images/tyson-landing.jpeg";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <section className="flex justify-center m-2">
-        <div className="md:w-4/5 h-64">
+      <section className="flex justify-center m-3">
+        <div className="container xl:w-3/5 h-screen/3">
+          <Jumbotron image={tysonLanding} title="This is Tys0n" fontSize="text-xl lg:text-2xl"/>
+        </div>
+      </section>
+
+      <section className="flex justify-center m-3">
+        <div className="container xl:w-3/5 h-screen/3">
+          <Jumbotron image={tysonLanding} title="This is Tys0n" fontSize="text-xl lg:text-2xl"/>
+        </div>
+      </section>
+
+      <section className="flex justify-center m-3">
+        <div className="container xl:w-3/5 h-screen/3">
+          <Jumbotron image={tysonLanding} title="This is Tys0n" fontSize="text-xl lg:text-2xl"/>
+        </div>
+      </section>
+
+      <section className="flex justify-center m-3">
+        <div className="container xl:w-3/5 h-screen/3">
           <Jumbotron image={tysonLanding} title="This is Tys0n" fontSize="text-xl lg:text-2xl"/>
         </div>
       </section>
