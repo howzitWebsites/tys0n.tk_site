369a52559542891e0f3ba3861fc8fb0f2b22be4d
Callum Fleming <howzitcallum@gmail.com>
2020-10-15 13:49:21 +0200

    feat(share): stating of a share button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import Jumbotron from "./components/Jumbotron";
 import ContentBlock from "./components/ContentBlock";
 
-import * as Icons from "heroicons-react";
+// import * as Icons from "heroicons-react";
+
+import {ReactComponent as Share} from "./icons/solid/share.svg";
 
 // import tysonLanding from "./images/tyson-landing.jpeg";
 import tyson_1 from "./images/tyson_1.jpg";
@@ -15,9 +17,9 @@ const App: React.FC = () => {
         <div className="h-screen/3 w-full lg:w-3/4 xl:w-3/4 xl:h-screen/2">
           <Jumbotron image={tyson_1} title="">
             <div className="flex">
-              <div className="h-full my-auto ml-3">
-                <Icons.Share size={28}/>
-              </div>
+              <button className="h-full my-auto ml-3" onClick={() => alert("share this!")}>
+                <Share className="h-8 md:h-10 text-gray-800 fill-current" />
+              </button>
               <h1 className="text-3xl md:text-4xl cursive text-gray-800 font-medium m-4 md:m-5">This is Tyson...</h1>
             </div>
           </Jumbotron>
@@ -27,7 +29,7 @@ const App: React.FC = () => {
       <section className="flex justify-center m-3 mt-6">
         <div className="h-screen/3 w-full lg:w-3/4 xl:w-3/4 xl:h-screen/2">
           <ContentBlock gradientDirection="tr">
-            <h1><Icons.PhoneMissedCall/> test</h1>
+            {/* <h1><Icons.PhoneMissedCall/> test</h1> */}
           </ContentBlock>
         </div>
       </section>
