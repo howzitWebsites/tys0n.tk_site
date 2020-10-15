b51a727043f75efeb3f9f6a752437d9a9f1030f0
Callum Fleming <howzitcallum@gmail.com>
2020-10-15 08:19:50 +0200

    feat(title edit): chaging the size of the title on mobile

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -11,7 +11,7 @@ const Jumbotron: FC<IJumbotronProps> = ({image, title, fontSize}) => {
     <div className="h-full w-full">
         <img src={image} className="object-cover w-full h-full rounded-lg shadow-lg" alt="Jumbotron"/>
         <div className="flex justify-center text-center -mt-20">
-            <h1 className={`bg-gradient-to-r from-teal-400 to-blue-500 w-4/5 md:w-1/2 p-5 rounded-lg shadow-lg ${fontSize}`}>{title}</h1>
+            <h1 className={`bg-gradient-to-r from-teal-400 to-blue-500 p-2 w-4/5 md:w-1/2 md:p-5 rounded-lg shadow-lg ${fontSize}`}>{title}</h1>
         </div>
     </div>
     );
