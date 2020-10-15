a6a9e5fcfb87c9da2643b410bec7badbfe72d9cb
Callum Fleming <howzitcallum@gmail.com>
2020-10-15 09:56:13 +0200

    feat(jumbotron): more colour adj and android top bar

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -9,9 +9,9 @@ interface IJumbotronProps {
 const Jumbotron: FC<IJumbotronProps> = ({image, title, titleClasses}) => {
     return (
     <div className="h-full w-full">
-        <img src={image} className="object-cover w-full h-full rounded-lg shadow-lg" alt="Jumbotron"/>
+        <img src={image} className="object-cover w-full h-full rounded-lg shadow-lg" alt={title}/>
         <div className="flex justify-center text-center -mt-8 md:-mt-10">
-            <h1 className={`bg-gradient-to-r from-white to-gray-500 p-3 w-4/5 md:w-1/2 md:p-5 rounded-lg shadow-lg ${titleClasses}`}>{title}</h1>
+            <h1 className={`bg-gradient-to-r from-green-300 to-gray-600 p-4 w-4/5 md:w-1/2 md:p-5 rounded-lg shadow-lg ${titleClasses}`}>{title}</h1>
         </div>
     </div>
     );
